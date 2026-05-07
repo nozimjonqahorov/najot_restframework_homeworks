87a124cb5da10e5555037504a27c91d389a0d52b
Nozimjon <126617482+nozimjonqahorov@users.noreply.github.com>
2026-05-07 15:30:01 +0500

    Delete library directory

--- library/phones/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Phone
-
-class PhoneSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Phone
-        fields = "__all__"
-        
